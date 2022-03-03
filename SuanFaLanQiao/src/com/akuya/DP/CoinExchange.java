a14ffdfcf849e71f31fe8e146bd1631ed1a01491
tanchenxu <1828504053@qq.com>
2022-03-03 23:47:50 +0800

    DPThird

--- SuanFaLanQiao/src/com/akuya/DP/CoinExchange.java
+++ SuanFaLanQiao/src/com/akuya/DP/CoinExchange.java
@@ -4,7 +4,54 @@ import java.util.Scanner;
 
 public class CoinExchange {
 	public static void main(String[] args) {
-		Scanner scanner =new Scanner(System.in)
+		Scanner scanner =new Scanner(System.in);
+		int n=scanner.nextInt();
+		int money=scanner.nextInt();
+		int coins[]=new int[n];
+		for(int i=0;i<n;i++){
+			coins[i]=scanner.nextInt();
+		}
+		System.out.println(choose(coins, n, money));
+		}
+	public static int choose(int []coins,int n,int money) {
+		int mery[]=new int[money+1];
+		for(int i=1;i<money+1;i++){
+			mery[i]=1000;
+		}
+		mery[0]=0;
+		for(int i=1;i<=money;i++){
+			int temp[]=new int[n];
+			boolean flag=false;
+			for(int j=0;j<n;j++){
+				
+				temp[j]=i-coins[j];
+				if(temp[j]>=0){
+					flag=true;
+				}
+				if(temp[j]<0){
+				    temp[j]=money;
+				}
+			}
+			/*if(flag==false){
+				return -1;
+			}*/
+			mery[i]=min(n, temp, mery)+1;
+		}
+		if(mery[money]<1000){
+		return mery[money];
+		}else{
+			return -1;
+		}
 	}
 
+	public static int min(int n,int temp[],int mery[]) {
+		int min=mery[temp[0]];
+		for(int i=1;i<n;i++){
+			if(mery[temp[i]]<min){
+				min=mery[temp[i]];
+			}
+		}
+		
+		return min;
+	}
 }
